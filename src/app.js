e9026621b556f7f532b157718b03d1540bfc961e
GSimmonds <lauderhillgb@gmail.com>
2024-10-17 13:29:32 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["their", "his"];
+  let adj = ["super", "giant"];
+  let noun = ["athlete", "dog"];
+
+  function domainGenerator() {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let r = 0; r < noun.length; r++) {
+          console.log(pronoun[i] + adj[j] + noun[r] + ".com");
+        }
+      }
+    }
+  }
+  domainGenerator();
 };
